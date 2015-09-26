324ffe47e0449900f53be6ab607b8807ff0dd37b
yousifd <yad999@gmail.com>
2015-09-25 19:14:50 -0700

    Add PointManager

---
+++ Assets/PointManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointManager : MonoBehaviour {
+
+	private List<Transform> points = new List<Transoform>();
+	public const Vector2 gravityConstant;
+	public const Vector2 attractionConstant;
+	public const Vector2 repulsionConstant;
+
+	void FixedUpdate(){
+		// loop through points
+		foreach (Transform point in points) {
+			// loop through other points
+			foreach (Transform otherPoint in points.Remove(point)) {
+				// Add Graivty Force to point
+				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+	
+				// Add attraction Force to point
+				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+
+				// Add repulsive Force to otherPoint
+				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+			}
+		}
+	}
+}

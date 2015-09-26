ac2a8dc7e42e14b0f673457b81f85624e7cf5918
yousifd <yad999@gmail.com>
2015-09-25 19:58:43 -0700

    Complete pointManager

--- Assets/PointManager.cs
+++ Assets/PointManager.cs
@@ -1,26 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PointManager : MonoBehaviour {
+	
+	public float gravityConstant = 1;
+	public float attractionConstant = 1;
+	public float repulsionConstant = 1;
 
-	private List<Transform> points = new List<Transoform>();
-	public const Vector2 gravityConstant;
-	public const Vector2 attractionConstant;
-	public const Vector2 repulsionConstant;
+	private List<Transform> points = new List<Transform>();
 
 	void FixedUpdate(){
 		// loop through points
 		foreach (Transform point in points) {
+			Rigidbody2D pointComponent = GetComponent<Rigidbody2D>();
+
+			// Add Graivty Force to point
+			pointComponent.AddForce (gravityConstant * Vector2.down);
+
+			// remove point from points to not apply focres to it
+			points.Remove(point);
+
 			// loop through other points
-			foreach (Transform otherPoint in points.Remove(point)) {
-				// Add Graivty Force to point
-				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+			foreach (Transform otherPoint in points) {
 	
 				// Add attraction Force to point
-				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+				Vector2 appliedForce = -attractionConstant* (pointComponent.velocity / (point.position - otherPoint.position).magnitude);
+				pointComponent.AddForce (appliedForce);
 
 				// Add repulsive Force to otherPoint
-				GetComponent<Rigidbody2D>().AddForce (/*Vector*/);
+				appliedForce = repulsionConstant * (pointComponent.velocity / (point.position - otherPoint.position).magnitude);
+				pointComponent.AddForce (appliedForce);
+				pointComponent.AddForce (repulsionConstant * Vector2.right);
+
+				points.Add(point);
 			}
 		}
 	}

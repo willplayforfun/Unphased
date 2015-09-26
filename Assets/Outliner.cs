6dcc62e5f268316efa44a4c7c686bbbec89ac58f
unknown <emlevoni@SCI_L113_23.sca.usc.edu>
2015-09-25 20:20:18 -0700

    First integration

---
+++ Assets/Outliner.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Outliner : MonoBehaviour
+{
+    void Update()
+    {
+        JarvisWrap();
+    }
+
+    public void JarvisWrap()
+    {
+
+        //Start with point with smallest y-coordinate.
+        PointManager pm = GetComponent<PointManager>();
+
+        Debug.Log(pm.points.Count);
+
+
+        Transform lowestPoint = pm.points[0];
+
+        foreach(Transform point in pm.points)
+        {
+            if(point.position.y < lowestPoint.position.y)
+            {
+                lowestPoint = point;
+            }
+        }
+
+        Debug.Log(lowestPoint.name);
+
+        Transform currentPoint = lowestPoint;
+        Vector3 lastEdge = Vector3.right;
+
+        int count = 0;
+        do
+        {
+            Transform bestPoint = null;
+            float bestAngle = Mathf.Infinity;
+
+            //Compute angle between current point and all remaining points.
+            //Pick smallest angle larger than current angle.
+            foreach (Transform point in pm.points)
+            {
+                if (!point.Equals(currentPoint))
+                {
+                    float CCWangle = Vector3.Angle(lastEdge, (point.position - currentPoint.position));
+
+                    Vector3 cross = Vector3.Cross(lastEdge, (point.position - currentPoint.position));
+                    if(cross.z < 0)
+                    {
+                        CCWangle = 360 - CCWangle;
+                    }
+
+                    if (bestPoint == null || CCWangle < bestAngle)
+                    {
+                        bestPoint = point;
+                        bestAngle = CCWangle;
+                    }
+                }
+            }
+
+            Debug.Log(bestPoint.name + " " + bestAngle);
+            Debug.DrawLine(currentPoint.position, bestPoint.position, Color.green);
+
+            //Repeat
+            lastEdge = (bestPoint.position - currentPoint.position);
+            currentPoint = bestPoint;
+
+            count++;
+
+        } while (!currentPoint.Equals(lowestPoint) && count < pm.points.Count);
+    }
+}
+
--- Assets/PointManager.cs
+++ Assets/PointManager.cs
@@ -8,32 +8,57 @@ public class PointManager : MonoBehaviour {
 	public float attractionConstant = 1;
 	public float repulsionConstant = 1;
 
-	private List<Transform> points = new List<Transform>();
+    public float repulsiveLimit;
+
+	public List<Transform> points = new List<Transform>();
+
+    public GameObject pointPrefab;
+
+    void Start()
+    {
+        List<GameObject> objs = new List<GameObject>(GameObject.FindGameObjectsWithTag("Point"));
+        foreach(GameObject obj in objs)
+        {
+            points.Add(obj.transform);
+        }
+        /*
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newPoint = Instantiate(pointPrefab);
+            newPoint.transform.position = this.transform.position + Vector3.up * Random.Range(-1.5f, 1.5f) + Vector3.right * Random.Range(-1.5f, 1.5f) ;
+            points.Add(newPoint.transform);
+        }
+        */
+    }
 
 	void FixedUpdate(){
 		// loop through points
 		foreach (Transform point in points) {
-			Rigidbody2D pointComponent = GetComponent<Rigidbody2D>();
+			Rigidbody2D pointComponent = point.GetComponent<Rigidbody2D>();
 
 			// Add Graivty Force to point
 			pointComponent.AddForce (gravityConstant * Vector2.down);
 
 			// remove point from points to not apply focres to it
-			points.Remove(point);
+			//points.Remove(point);
+
 
 			// loop through other points
 			foreach (Transform otherPoint in points) {
-	
-				// Add attraction Force to point
-				Vector2 appliedForce = -attractionConstant* (pointComponent.velocity / (point.position - otherPoint.position).magnitude);
-				pointComponent.AddForce (appliedForce);
 
-				// Add repulsive Force to otherPoint
-				appliedForce = repulsionConstant * (pointComponent.velocity / (point.position - otherPoint.position).magnitude);
-				pointComponent.AddForce (appliedForce);
-				pointComponent.AddForce (repulsionConstant * Vector2.right);
+                if (!point.Equals(otherPoint))
+                {
+                    Vector2 distance = (point.position - otherPoint.position);
 
-				points.Add(point);
+
+                    // Add attraction Force to point
+                    pointComponent.AddForce(-attractionConstant * Vector2.ClampMagnitude((distance.normalized / Mathf.Pow(distance.magnitude, 2)), repulsiveLimit));
+
+                    // Add repulsive Force to otherPoint
+                    pointComponent.AddForce(repulsionConstant * Vector2.ClampMagnitude((distance.normalized / Mathf.Pow(distance.magnitude, 3)), repulsiveLimit));
+                    //pointComponent.AddForce (repulsionConstant * Vector2.right);
+                }
+				//points.Add(point);
 			}
 		}
 	}
